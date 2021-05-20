216b2f3d9a944c656e116ef6cfbdbf429202c037
vivek yadav <vivek.yadavchamp@gmail.com>
2021-05-20 13:09:29 +0530

    Tessellation using cursor position

--- ReverseTessellation/main.cpp
+++ ReverseTessellation/main.cpp
@@ -8,8 +8,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
 void processInput(GLFWwindow* window);
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 1.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -52,33 +53,38 @@ int main()
            // std::cout << temp1 << " " << temp2 << " " << temp3 << " " << temp4 << "\n";
             terrain.push_back(temp1);
             terrain.push_back(temp2);
+            terrain.push_back(0.2f);
             
             terrain.push_back(temp3);
             terrain.push_back(temp2);
+            terrain.push_back(0.3f);
 
             terrain.push_back(temp1);
             terrain.push_back(temp4);
+            terrain.push_back(0.4f);
 
             terrain.push_back(temp3);
             terrain.push_back(temp2);
+            terrain.push_back(0.2f);
 
             terrain.push_back(temp1);
             terrain.push_back(temp4);
+            terrain.push_back(0.8f);
 
             terrain.push_back(temp3);
             terrain.push_back(temp4);
+            terrain.push_back(1.0f);
+
         }
     }
     /*for (int i = 0; i < 6 * 2 * 100 * 100; i+= 6) {
         std::cout << terrain[i] << " " << terrain[i + 1] << " " << terrain[i + 2] << " "
             << terrain[i + 3] << " " << terrain[i + 4] << " " << terrain[i + 5] << "\n";
     }*/
-    std::vector<float> triangle = {
-         0.75, -0.75, 0.5, 1.0,
-        -0.75, -0.75, 0.5, 1.0,
-         0.75, 0.75, 0.5, 1.0
+    float triangle[] = { 0.5f, 0.5f, -0.5f, 0.2f, 0.6f, 0.1f,
+                         0.0f, 0.0f, -0.5f, 0.8f, 0.8f, 0.2f,
+                         0.0f, 0.5f, -0.5f, 0.1f, 0.0f, 0.4f
     };
-
     unsigned int vao;
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
@@ -86,50 +92,77 @@ int main()
     unsigned int vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 2 * 40 * 40 * 6 * sizeof(float), &terrain[0] , GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3  * 40 * 40 * 6 * sizeof(float), &terrain[0] , GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))); 
-    //glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(2 * sizeof(float))); 
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(vao);
 
     const char* vertexShaderSource =
-        R"GLSL(#version 410 core
+        R"GLSL(
+        #version 410 core
         layout (location = 0) in vec2 aPos;
+        layout (location = 1) in float aTfac;
         uniform mat4 model;
         uniform mat4 view;
         uniform mat4 projection;
-       // "out vec3 vPosition;"
+        flat out float tFactor;
         void main()
         {
-        const vec4 vertices[] = vec4[](vec4(0.75, -0.75, 0.5, 1.0),
-            vec4(-0.75, -0.75, 0.5, 1.0),
-            vec4(0.75, 0.75, 0.5, 1.0));
-               //gl_Position = vertices[gl_VertexID];
-        gl_Position =   projection * view * model  * vec4(aPos, 0.5, 1.0);
-        })GLSL";
 
+            tFactor = aTfac;
+            gl_Position =   projection * view * model  * vec4(aPos, 0.5, 1.0);
+        }
+        )GLSL";
+    
     const char* tesscontrolShaderSource =
         R"glsl(
         #version 410 core
         layout(vertices = 3) out;
-    	//in vec3 vPosition[];
-    	//out vec3 tcPosition[];
-    	//uniform float TessLevelInner
-    	//uniform float TessLevelOuter; 
+                
+        const int AB = 2;
+        const int BC = 3;
+        const int CD = 0;
+        const int DA = 1;
+    	uniform double xpos;
+        uniform double ypos;
+    	flat in float TFactor[];
+    	float TSlope = 1.8;
+    	float TShift = 0.3;
+    	
+        float fac = 600;
+        float LodFactor(float dist, int i){
+            float tessellationLevel = max(0.0, 600/pow(dist, TSlope) + TShift);
+            return tessellationLevel; 
+        }
         
         void main()
         {
-        	//tcPosition[ID] = vPosition[ID];
+            int i = 0;
             if (gl_InvocationID == 0) {
-                gl_TessLevelInner[0] = 2.0;
-                gl_TessLevelOuter[0] = 1.0;
-                gl_TessLevelOuter[1] = 1.0;
-                gl_TessLevelOuter[2] = 1.0;
+                vec3 abMid = vec3(gl_in[0].gl_Position  + gl_in[3].gl_Position)  / 2.0;
+                vec3 bcMid = vec3(gl_in[3].gl_Position  + gl_in[15].gl_Position) / 2.0;
+                vec3 cdMid = vec3(gl_in[15].gl_Position + gl_in[12].gl_Position) / 2.0;
+                vec3 daMid = vec3(gl_in[12].gl_Position + gl_in[0].gl_Position)  / 2.0;
+                
+                float distanceAB = distance(abMid, vec3(xpos, ypos, 0.5));
+                float distanceBC = distance(bcMid, vec3(xpos, ypos, 0.5));              
+                float distanceCD = distance(cdMid, vec3(xpos, ypos, 0.5));              
+                float distanceDA = distance(daMid, vec3(xpos, ypos, 0.5));              
+                              
+                gl_TessLevelOuter[AB] = mix(i, gl_MaxTessGenLevel,  LodFactor(distanceAB, i));
+                gl_TessLevelOuter[BC] =  mix(i, gl_MaxTessGenLevel, LodFactor(distanceBC, i));
+                gl_TessLevelOuter[CD] =  mix(i, gl_MaxTessGenLevel, LodFactor(distanceCD, i));
+                gl_TessLevelOuter[DA] =  mix(i, gl_MaxTessGenLevel, LodFactor(distanceDA, i));
+                i++;
+                //gl_TessLevelInner[0] = (gl_TessLevelOuter[BC] + gl_TessLevelOuter[DA])/4;
+                //gl_TessLevelInner[1] = (gl_TessLevelOuter[AB] + gl_TessLevelOuter[CD])/4;
+                                
             }
             gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
         }
@@ -144,22 +177,19 @@ int main()
             {
                 gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position) +
                               (gl_TessCoord.y * gl_in[1].gl_Position) +
-                              (gl_TessCoord.z * gl_in[2].gl_Position);
+                              (gl_TessCoord.z * gl_in[2].gl_Position) ;
             }
         )glsl";
-
+    
     const char* fragmentShaderSource =
         R"glsl(
             #version 410 core
-            out vec4 FragColor;
-           
+            out vec4 FragColor;     
             void main()
             {
-                
-
-                FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+                FragColor = vec4(1.0, 0.5, 0.5, 1.0);
             }
-            )glsl";
+       )glsl";
 
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -199,7 +229,7 @@ int main()
         glGetShaderInfoLog(tessEvalShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::TESSEVAL::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
-
+    
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -224,7 +254,8 @@ int main()
     glDeleteShader(tessControlShader);
     glDeleteShader(tessEvalShader);
     glDeleteShader(fragmentShader);
-
+    //float tfac = 0.2f;
+    double xpos = 0, ypos = 0;
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         processInput(window);
@@ -237,12 +268,22 @@ int main()
         glUniformMatrix4fv(projection_loc, 1, GL_FALSE, glm::value_ptr(projection));
         
         //TessLevelInner
-      //  unsigned int innerTess = glGetUniformLocation(shaderProgram, "TessLevelInner");
+        //unsigned int innerTess = glGetUniformLocation(shaderProgram, "TessLevelInner");
         //glUniform1f(innerTess, 4.0f);
         
         //unsigned int outerTess = glGetUniformLocation(shaderProgram, "TessLevelOuter");
         //glUniform1f(outerTess, 4.0f);
-        
+        glfwGetCursorPos(window, &xpos, &ypos);
+        //std::cout << xpos << "  " << ypos << "\n";
+        //unsigned int tessFactor = glGetUniformLocation(shaderProgram, "Tfactor");
+        //glUniform1f(tessFactor, tfac);
+        //tfac += 0.1f;
+        //if (tfac > 3.0f) tfac = 0.2f;
+
+        unsigned int cursorxpos = glGetUniformLocation(shaderProgram, "xpos");
+        glUniform1d(cursorxpos, xpos);
+        unsigned int cursorypos = glGetUniformLocation(shaderProgram, "ypos");
+        glUniform1d(cursorypos, ypos);
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         unsigned int view_loc = glGetUniformLocation(shaderProgram, "view");
         glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm::value_ptr(view));
@@ -251,7 +292,7 @@ int main()
         unsigned int postions_loc = glGetUniformLocation(shaderProgram, "model");
         glUniformMatrix4fv(postions_loc, 1, GL_FALSE, glm::value_ptr(model));
         
-        glDrawArrays(GL_PATCHES, 0, 6 * 100 * 100);
+        glDrawArrays(GL_PATCHES, 0, 2 * 4 * 100 * 100);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -273,3 +314,4 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
+
